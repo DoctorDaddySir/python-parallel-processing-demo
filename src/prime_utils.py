2356e4539d1375412caff4abd97928a4e853e695
Trent Shelton <perry.t.shelton@gmail.com>
2026-04-09 12:17:29 -0500

    add shared utility script - prime_utils.py

---
+++ src/prime_utils.py
@@ -0,0 +1,21 @@
+def is_prime(n: int) -> bool:
+    """
+    Deliberately inefficient prime checker for demonstration CPU-bound workloads
+    """
+    if n<2:
+        return False
+    for i in range(2, n):
+        if n%i == 0:
+            return False
+    return True
+
+def count_primes_in_range(start: int, end: int) -> int:
+    """
+    Count How many prime numbers exist in half-open interval [start, end)
+    """
+    count = 0
+    for n in range(start, end):
+        if is_prime(n):
+            count += 1
+
+    return count
